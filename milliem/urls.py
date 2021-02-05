e1dc648ba737d47249470f362fd44e50bae37e65
이혜성 <hyeseong43@gmail.com>
2021-02-05 12:06:38 +0900

    FINAL COMMIT FOR DEPLOYMENT

--- milliem/urls.py
+++ milliem/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users', include('users.urls')),
+    path('book', include('book.urls')),
 #    path('library', include('library.urls'))
 ]
